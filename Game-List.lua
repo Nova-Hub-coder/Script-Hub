515caf3d6cfaa8b0bf71e794c09f4ebceefcb8e8
Moxiaobai <auroralittle@qq.com>
2025-09-28 11:03:49 +0800

    Create Game-List.lua

---
+++ Game-List.lua
@@ -0,0 +1,8 @@
+return {
+    ["Dungeon Heroes"] = "https://raw.githubusercontent.com/Nova-Hub-coder/Script-Hub/refs/heads/main/obf/Dungeon%20Heroes-obfuscated.lua",
+    ["War Tycoon"] = "https://raw.githubusercontent.com/Nova-Hub-coder/Script-Hub/refs/heads/main/obf/WarTycoon-obfuscated.lua",
+    ["Volleyball Legend"] = "https://raw.githubusercontent.com/Nova-Hub-coder/Script-Hub/refs/heads/main/obf/Volleyball%20Legend-obfuscated.lua",
+    ["Hyper Shot"] = "https://raw.githubusercontent.com/Nova-Hub-coder/Script-Hub/refs/heads/main/obf/Hypershot-obfuscated.lua",
+    ["steal cookie"] = "https://raw.githubusercontent.com/Nova-Hub-coder/Script-Hub/refs/heads/main/obf/Steal%20Cookie-obfuscated.lua",
+    ["Fish it"] = "https://raw.githubusercontent.com/Nova-Hub-coder/Script-Hub/refs/heads/main/obf/Fish-obfuscated.lua",
+}
